90b73eab21924b38d8b60223c6819635d6593b8b
woodmawa <will.woodman@btinternet.com>
2021-11-19 16:18:06 +0000

    look inside class extending expando

--- src/main/groovy/script/testExpandoAndMetaProgramming.groovy
+++ src/main/groovy/script/testExpandoAndMetaProgramming.groovy
@@ -9,11 +9,17 @@ MyClass2 myc = new MyClass2()
 
 myc.name = "william"
 myc.dynamic = "dynamic"
-myc.metaClass.static.getStaticDynamic = {"static dynamic value"}
+myc.metaClass.static.getStaticDynamicMethod = {"static dynamic closure value"}
+myc.metaClass.static.staticDynamicProperty = "static dynamic property value"
 
 //if you know the name of the closure method you can get its value
-def res = myc.metaClass.getStaticMetaMethod("getStaticDynamic", [] as Object[]).doMethodInvoke(myc, [] as Object[])
+def res = myc.metaClass.getStaticMetaMethod("getStaticDynamicMethod", [] as Object[]).doMethodInvoke(myc, [] as Object[])
 println "myc get static method : [$res]"
+
+//no means to get this property
+def propRes = myc.metaClass.getStaticMetaMethod("getStaticDynamicProperty", [] as Object[]).doMethodInvoke(myc, [] as Object[])
+println "myc get static property : [$propRes]"
+
 println "myc meta methods : " + myc.metaClass.metaMethods.collect {it.name}
 println "myc metaClass properties : "+ myc.metaClass.properties.collect {it.name}
 
