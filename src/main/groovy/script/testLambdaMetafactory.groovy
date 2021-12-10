f444ea5b946f801314e087202207a01d2096857d
woodmawa <will.woodman@btinternet.com>
2021-12-10 17:32:43 +0000

    doing own expando

--- src/main/groovy/script/testLambdaMetafactory.groovy
+++ src/main/groovy/script/testLambdaMetafactory.groovy
@@ -7,15 +7,16 @@ import java.lang.invoke.MethodHandle
 import java.lang.invoke.MethodHandles
 import java.lang.invoke.MethodType
 import java.lang.reflect.Method
+import java.util.function.Function
 import java.util.function.Supplier
 
 
 
 class ExampleClass {
-
-    ExampleClass() {}
-
     private String value = "hello"
+
+    ExampleClass() {}  //constructor
+
     String getValue () {return value}
     void setValue (String val) {value = val}
 }
@@ -26,19 +27,19 @@ MethodHandles.Lookup lookup= MethodHandles.lookup()
 
 
 // 2. Creates a MethodType
-MethodType sourceMethodType = MethodType.methodType(String.class, []);
+//MethodType sourceMethodType = MethodType.methodType(String.class, []);
 //                                            ^-----------^        ^----------^
 //                                             return type         argument class
 
 // 3. Find the MethodHandle
-MethodHandle metaHandle = lookup.findVirtual(ExampleClass.class, "getValue", sourceMethodType);
+//MethodHandle metaHandle = lookup.findVirtual(ExampleClass.class, "getValue", sourceMethodType);
 //                                       ^----------^               ^-------------^
 //                                            |                     name of method
 //                             class from which method is accessed
 
 // 4. Invoke the method
-String strVal
-strVal = (String) metaHandle.bindTo(instance).invokeWithArguments()  // this works
+//String strVal
+//strVal = (String) metaHandle.bindTo(instance).invokeWithArguments()  // this works
 //                                  ^----^                  ^----^
 //                                    |                    argument
 //                       instance your class  to invoke the method on
@@ -58,24 +59,25 @@ assert handle.toString() == virtRef.toString()
 
 MethodType mt = handle.type()
 
-MethodType invokedMethodType = MethodType.methodType(Supplier.class, []);
+//getter can be treated as Function where the compiler injects this reference as hiddden arg
+MethodType factoryMethodType = MethodType.methodType(Function.class, ExampleClass)
 
 java.lang.invoke.CallSite callSite = LambdaMetafactory.metafactory(
          lookup,
         //invoked name, name of method on Supplier interface
-        "get",
+        "getValue",
         //invokedType: expected signature of the callsite, The parameter types represent the types of capture variables, here invoked arg is Closure and returns Supplier
         //                   -- ret type --   -- invoked type -- on bindTo
-        invokedMethodType,
+        factoryMethodType,
         //MethodType.methodType(Supplier.class, []),
         // samMthodType: signature and return type of method to be implemented  by the function object, type erasure, Supplier will return an Object
         MethodType.methodType (Object.class),
         //implMethod handle that does the work - the handle for closure call()
-        handle,  //lookup.findVirtual(ExampleClass.class, "getValue", MethodType.methodType (String.class)),
+        virtRef, //handle,  //lookup.findVirtual(ExampleClass.class, "getValue", MethodType.methodType (String.class)),
         //instantiatedMethodType: signature and return type that should be forced dynamically at invocation.
         //This may be the same as samMethodType, or may be a specialization of it.
         //supplier method real signature  accepts no params and returns string
-        MethodType.methodType(Supplier.class)
+        MethodType.methodType(String.class)
 )
 
 MethodHandle factory = callSite.getTarget()
