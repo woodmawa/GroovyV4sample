b2f6c9b4d81a6e8447e85013d35aacad5137bfd5
woodmawa <will.woodman@btinternet.com>
2021-12-16 11:54:22 +0000

    doing own expando

---
+++ src/test/groovy/LambdaMetaclassTests/LambdaBeanClassAccessTests.groovy
@@ -0,0 +1,14 @@
+package LambdaMetaclassTests
+
+import extensible.WillsTestSubExpando
+import org.junit.jupiter.api.BeforeEach
+
+class LambdaBeanClassAccessTests {
+
+    Example
+    @BeforeEach
+    void init() {
+        wtse = new WillsTestSubExpando()
+    }
+
+}
