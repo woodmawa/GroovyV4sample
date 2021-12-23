6d7a076b92394056f9e534cf7620a3c35be99377
woodmawa <will.woodman@btinternet.com>
2021-12-23 16:06:24 +0000

    doing own expando

--- src/test/groovy/groovyGDK/GroovyTestCaseTest.groovy
+++ src/test/groovy/groovyGDK/GroovyTestCaseTest.groovy
@@ -1,7 +1,10 @@
 package groovyGDK
 
+
 import groovy.test.GroovyTestCase
 import org.junit.jupiter.api.Test
+
+import java.lang.reflect.Modifier
 
 /**
  * tests using GroovyTestCase, which includes standard assertions for you
@@ -23,5 +26,36 @@ class GroovyTestCaseTest extends GroovyTestCase {
         String  exception = shouldFail {1/0}
         shouldFail (Exception) {1/0}
         assert exception == "Division by zero"
+    }
+
+    @Test
+    void testMetaClassStatic () {
+
+        Integer num = 1
+        num.metaClass.static.getStatProp = {"dynamic static property"}
+
+        shouldFail (MissingPropertyException) {num.statProp == "dynamic static property"}
+
+        def mm = num.metaClass.getStaticMetaMethod('getStatProp', Object[])
+        assertTrue mm.isStatic()
+        assertEquals mm.invoke(num, []), "dynamic static property"
+
+        // but cant'ask for a list of staticMetaMethods - you have to know the name in advance
+
+        //ask for hasProperty  - says there is a prop called statProp
+        MetaProperty mp = num.hasProperty('statProp')
+        assertTrue Modifier.isStatic (mp.modifiers)
+        assertTrue num.hasProperty ('statProp').getProperty(num) == "dynamic static property"
+
+        // num.metaClass.getStaticMetaProperty ('statProp')  - method doesnt exist
+
+        //try and get properties - and statProp is not there
+        Map props  = num.getProperties()
+        assert props.size() == 1
+        assert props.get ('class')  == Integer
+        assert props.get ('statProp') == null
+        mp = props.find{it.key == 'statProp'}
+        assert mp == null
+
     }
 }
