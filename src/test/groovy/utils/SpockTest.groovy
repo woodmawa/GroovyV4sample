6268c575d1bd38b8bac8138fa23ce1ea3f1404f1
woodmawa <will.woodman@btinternet.com>
2021-12-23 11:57:34 +0000

    doing own expando

--- src/test/groovy/utils/SpockTest.groovy
+++ src/test/groovy/utils/SpockTest.groovy
@@ -8,4 +8,28 @@ class SpockTest extends Specification {
         expect:
         1 + 1 == 2
     }
+
+    def "collection methods results checking " () {
+        expect:
+        def list = [1,2,3,3,4,5,5,6]
+
+        list.findResults { it % 2} == [1, 0, 1, 1, 0, 1, 1, 0]
+
+        list.take(3) == [1,2,3]
+
+        list.findAll {it % 2 == 0 } == [2,4,6]
+
+        list.takeWhile {it < 4 } == [1,2,3,3]
+
+        list.grep {it % 2 == 0} == [2,4,6]
+
+        list.inject (0) { sum, it -> sum + it} == 29
+
+        list.toSet() == new HashSet([1,2,3,3,4,5,5,6])
+
+        list.unique() == [1,2,3,4,5,6]
+
+        list - [4,5,6] == [1,2,3]  // removes all numbers from the sublist
+
+    }
 }
