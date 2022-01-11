59d860ca8870eb3e9700b64cae7155cf78557420
woodmawa <will.woodman@btinternet.com>
2022-01-11 16:10:58 +0000

    doing own expando

--- src/main/groovy/script/trySimpleBenchmark.groovy
+++ src/main/groovy/script/trySimpleBenchmark.groovy
@@ -2,12 +2,24 @@ package script
 
 import benchmark.Benchmark
 
-Benchmark.nanoTimeit ("for loop ", 100) {
+import java.util.stream.Stream
+
+Benchmark.nanoTimeit ("for loop ", 1000) {
+    int sum = 0
     for (int i=0; i<100; i++) {
-        return  i
+        sum++
     }
 }
 
-Benchmark.nanoTimeit ("groovy each{} ", 100) {
-    100.each {return it}
+
+Benchmark.nanoTimeit ("groovy each{} ", 1000) {
+    int sum = 0
+
+    100.each {sum++}
+}
+
+Benchmark.nanoTimeit ("stream process ", 1000) {
+    int sum = 0
+
+    Stream.of(0..9).forEach(i -> sum+=1)
 }
