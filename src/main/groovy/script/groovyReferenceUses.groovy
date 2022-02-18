83861b5d65669fbc1633825f132ab047d45cbe44
woodmawa <will.woodman@btinternet.com>
2022-02-18 14:39:42 +0000

    update to groovy release dependency

---
+++ src/main/groovy/script/groovyReferenceUses.groovy
@@ -0,0 +1,15 @@
+package script
+
+Reference ref = new Reference()
+
+assert ref.get() == null
+
+ref.set({"closure return"})
+
+Closure clos = ref.get()
+
+assert clos
+assert clos() == "closure return"
+
+//clever recognises contained object and delegates the invokeMethod to that !
+ref.invokeMethod('call', []) == "closure return"
