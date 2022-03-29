a804cc374b04a81c9ae6b6272fecd0706a4505df
woodmawa <will.woodman@btinternet.com>
2022-03-29 18:41:58 +0100

    PointMap and tests

--- src/main/groovy/script/databaseApproach/gormDomainProxyWrapper.groovy
+++ src/main/groovy/script/databaseApproach/gormDomainProxyWrapper.groovy
@@ -5,7 +5,7 @@ GormDomainProxy proxyClass = new GormDomainProxy(DomainClass)
 GormDomainProxy proxyInst = proxyClass.newInstance()
 
 println proxyInst.propx
-//with's delegate is the DomainClass, so you can access its props/methods directly in context 
+//with's delegate is the DomainClass, so you can access its props/methods directly in context
 proxyInst.with {
     println "inside with(), propx is $propx "
 }//hello is proxied to gorm instance
